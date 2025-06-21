90f37cfdc9130834c55761a01bd7b5c30d8f2a8b
afuafu449 <hhanah703@gmail.com>
2025-06-21 13:29:51 +0530

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,11 @@
+/* Replace the object below with your own config
+   (from Firebase Console ▸ Project Settings ▸ Web App snippet)
+*/
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_ID.firebaseapp.com",
+  projectId: "YOUR_ID",
+  storageBucket: "YOUR_ID.appspot.com",
+  messagingSenderId: "....",
+  appId: "...."
+});
